530f94fe0a589f580e789d27f5021c239680fdb4
akbarmaul <lanang0995@gmail.com>
2019-12-15 15:54:13 +0700

    change port to env port

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)});
 app.put('/image', (req,res) => {image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)});
 
-app.listen(3010, () => {
-    console.log('app is running in port 3010');
+app.listen(process.env.PORT || 3010, () => {
+    console.log(`app is running in port ${process.env.PORT}`);
 });
